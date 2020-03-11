8b15c27f8f29bf10bf84c1a9172feba537440088
taoqf <tao_qf@163.com>
2020-03-11 17:38:18 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
+import moment from 'moment';
 
-export default function an11(param1: string) {
+/**
+ * 获取某天的开始时间
+ * @param time 时间戳
+ */
+export default function start_day(time: string, format: string): number;
+export default function start_day(time: number): number;
+export default function start_day(time: number | string, format?: string) {
+	return moment(time, format).startOf('day').valueOf();
 }
--- tests/test.js
+++ tests/test.js
@@ -2,6 +2,12 @@ const test = require('ava');
 
 const { default: a } = require('../dist/index');
 
-test('xxx', (t) => {
-	t.pass();
+test('获取某天的开始时间 时间格式字符串', (t) => {
+	const start = a('2019/09/18', 'YYYY/MM/DD');
+	t.is(start, 1568736000000);
+});
+
+test('获取某天的开始时间 时间戳', (t) => {
+	const start = a(1568736236548);
+	t.is(start, 1568736000000);
 });
